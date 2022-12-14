89567f57c699a4279c4d6d267b4572e7cae0846f
Anton Bezbrodoy <anton.bezborody@gmail.com>
2022-12-14 18:55:22 +0400

    firebase init

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,44 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0vKm94QPlMQaOYFOPSGdrT9kLyHQdEbE",
+  authDomain: "streethead-db.firebaseapp.com",
+  projectId: "streethead-db",
+  storageBucket: "streethead-db.appspot.com",
+  messagingSenderId: "152880453219",
+  appId: "1:152880453219:web:b39c034762f99397b7ca56",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+};
