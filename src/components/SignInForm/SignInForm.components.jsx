b043f94afd39edba0d9b162310ea28b34155d551
Anton Bezbrodoy <anton.bezborody@gmail.com>
2022-12-16 17:37:34 +0400

    auth page implement

---
+++ src/components/SignInForm/SignInForm.components.jsx
@@ -0,0 +1,108 @@
+import { useState } from "react";
+
+import FormInput from "../FormInput/FormInput.component";
+import Button from "../Button/Button.component";
+
+import "./SignInForm.styles.scss";
+
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword,
+} from "../../utils/firebase/firebase.utils";
+
+///
+
+const SignInForm = () => {
+  // redirect example
+  // useEffect(() => {
+  //   const getDatas = async () => {
+  //     const response = await getRedirectResult(auth);
+  //     if (response) {
+  //       const userDocRef = await createUserDocumentFromAuth(response.user);
+  //     }
+  //   };
+  //   getDatas();
+  // }, []);
+
+  const defaultFormFields = {
+    email: "",
+    password: "",
+  };
+
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const resetForm = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const onFormSubmitHandle = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await signInAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(response);
+      resetForm();
+    } catch (error) {
+      switch (error.code) {
+        case "auth/wrong-password":
+          alert("Incorrect password!");
+          break;
+        case "auth/user-not-found":
+          alert("User does not exist");
+          break;
+        default:
+          console.error(error);
+          break;
+      }
+    }
+  };
+
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(user);
+  };
+
+  return (
+    <div className="sign-in-container">
+      <h2>Already have an account?</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={onFormSubmitHandle}>
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          onChange={onChangeHandler}
+          name="email"
+          value={email}
+        />
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          onChange={onChangeHandler}
+          name="password"
+          value={password}
+        />
+        <div className="buttons-container">
+          <Button type="submit">Sign in</Button>
+          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
+            Google sign in
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignInForm;
