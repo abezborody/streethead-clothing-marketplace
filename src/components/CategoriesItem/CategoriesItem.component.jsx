f9c7ba1c14f668066ea8565e6fa8696fc87d66fe
Anton Bezbrodoy <anton.bezborody@gmail.com>
2022-12-27 19:48:25 +0400

    Minor fixes

--- src/components/CategoriesItem/CategoriesItem.component.jsx
+++ src/components/CategoriesItem/CategoriesItem.component.jsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom";
+
 import {
   BackgroundImage,
   Body,
@@ -5,9 +7,12 @@ import {
 } from "./CategoriesItem.styles.jsx";
 
 const CategoriesItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
   return (
-    <CategoriesItemContainer>
+    <CategoriesItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
